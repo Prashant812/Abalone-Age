1eb68f0de9a8f0c0b661a0e5e4d74bd0c307abb8
Prashant Kumar <93676625+Prashant812@users.noreply.github.com>
2022-08-25 23:34:10 +0530

    make it better

--- app.py
+++ app.py
@@ -11,22 +11,18 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
+
     int_features = [float(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
     prediction = model.predict(final_features)
 
-    output = round(prediction[0], 2)
+    output = round(prediction[0])
 
-    return render_template('index.html', prediction_text='Employee Salary should be  {}'.format(output))
+    return render_template('index.html', prediction_text='Abalone age should be  {}'.format(output))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
-    '''
-    For direct API calls trought request
-    '''
+    
     data = request.get_json(force=True)
     prediction = model.predict([np.array(list(data.values()))])
 
